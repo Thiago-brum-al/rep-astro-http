26e5d27ee4549f93106aeb46e5993469719132de
Thiago Brum <thiagobrum222@gmail.com>
2026-09-02 20:30:31 -0300

    first commit

---
+++ src/pages/api/posts/[slug].ts
@@ -0,0 +1,93 @@
+import type { APIRoute } from "astro";
+import { getEntry } from "astro:content";
+
+export const GET: APIRoute = async ({ params, request }) => {
+    const { slug } = params;
+    const post = await getEntry('blog', slug as any);
+
+    if(!post) return new Response(
+        JSON.stringify({
+            message: "Not found"
+        }), 
+        { 
+            status: 404
+        }
+    );
+
+    return new Response(
+        JSON.stringify({
+            method: "GET",
+            ...post
+        }), 
+        { 
+            status: 200,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+    );
+}
+
+export const POST:APIRoute = async ({ params, request }) => {
+    const body = await request.json();
+    return new Response(
+        JSON.stringify({
+            method: "POST",
+            ...body
+        }),
+        {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        } 
+    )
+}
+
+export const PUT:APIRoute = async ({ params, request }) => {
+    const body = await request.json();
+    return new Response(
+        JSON.stringify({
+            method: "PUT",
+            ...body
+        }),
+        {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        } 
+    )
+}
+
+export const PATCH:APIRoute = async ({ params, request }) => {
+    const body = await request.json();
+    return new Response(
+        JSON.stringify({
+            method: "PATCH",
+            ...body
+        }),
+        {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json"
+            }       
+        } 
+    )
+}
+
+export const DELETE:APIRoute = async ({ params, request }) => {
+    const body = await request.json();
+    return new Response(
+        JSON.stringify({
+            method: "DELETE",
+            ...body
+        }),
+        {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        } 
+    )
+}
